74af2927b73fa8a4003debbc01680b504c3a472e
user <user@user>
2022-03-29 20:05:46 -0600

    Updated colors, added welcome message with instructions

--- app/src/main/java/com/harr1424/listmaker/MainActivity.kt
+++ app/src/main/java/com/harr1424/listmaker/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.harr1424.listmaker
 
+import android.content.SharedPreferences.Editor
 import android.os.Bundle
+import android.preference.PreferenceManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -12,6 +16,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val welcomeShown = prefs.getBoolean("PREV_SHOWN", false)
+        if (!welcomeShown) {
+            val title = "Welcome!"
+            val instructions = "Add a new item by clicking the add button at the bottom of the " +
+                    "screen.\n\nDelete item(s) by clicking and holding them."
+            AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(instructions)
+                .setPositiveButton(
+                    "Got It"
+                ) { dialog, _ -> dialog.dismiss() }
+                .show()
+            val editor: Editor = prefs.edit()
+            editor.putBoolean("PREV_SHOWN", true)
+            editor.apply()
+        }
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
